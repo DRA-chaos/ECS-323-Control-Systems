f8bec4889999cdb85a9d0cc98ad7fbe421e63d0a
Rita Abani <ritapunk9@gmail.com>
2021-09-25 21:28:33 +0530

    Add files via upload

---
+++ Assignment 1/springParam.py
@@ -0,0 +1,19 @@
+# Spring Mass Damper Parameter File
+#Rita Abani 19244
+import numpy as np
+# import control as cnt
+
+# Physical parameters of the arm known to the controller
+m = 4.922     # Mass of the arm, kg
+b = 0.495      # Damping coefficient, Nms
+k = 2.989		# Spring constant
+
+# Simulation Parameters
+t_start = 0.0  # Start time of simulation
+t_end = 50.0  # End time of simulation
+Ts = 0.01  # sample time for simulation
+t_plot = 0.1  # the plotting and animation is updated at this rate
+
+
+               
+
